38971cd1f5349f96e73d732ae184a15522b42324
Andreas Braatz <andreas@fksfl.de>
2015-03-06 08:58:13 +0100

    Initial Commit

---
+++ OpenWeatherCall/OpenWeatherCall.h
@@ -0,0 +1,19 @@
+//
+//  OpenWeatherCall.h
+//  OpenWeatherCall
+//
+//  Created by Arbeit on 06.03.15.
+//  Copyright (c) 2015 Andreas Braatz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenWeatherCall.
+FOUNDATION_EXPORT double OpenWeatherCallVersionNumber;
+
+//! Project version string for OpenWeatherCall.
+FOUNDATION_EXPORT const unsigned char OpenWeatherCallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenWeatherCall/PublicHeader.h>
+
+
